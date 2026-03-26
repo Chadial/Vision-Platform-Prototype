d062c4718319ebd38f05af0cd08226fd5cc7c742
Chadial <chad@chad.net>
2026-03-26 20:12:10 +0100

    refactor: move control implementation behind vision_platform

--- src/vision_platform/control/__init__.py
+++ src/vision_platform/control/__init__.py
@@ -1,3 +1,5 @@
+"""Host-facing control surfaces exposed through the platform namespace."""
+
 from vision_platform.control.command_controller import CommandController
 
 __all__ = ["CommandController"]
